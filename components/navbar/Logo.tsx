de4c8f83ebdc0cda42f8feff15f80844deab70b9
Gurin Mihail <73294844+MikeXado@users.noreply.github.com>
2023-06-29 13:09:54 +0300

    Update Logo.tsx

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Logo({ logo }: { logo: string }) {
   const com = logo.substring("GeekifiedMike".length);
-  const name = logo.substring(0, 12);
+  const name = logo.substring(0, 13);
 
   return (
     <Link href="/" className="text-white text-lg font-extrabold">
