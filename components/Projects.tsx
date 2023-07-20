874fb7208e11b1aa695bab9e963bd30429d98547
Gurin Mihail <73294844+MikeXado@users.noreply.github.com>
2023-07-20 19:41:31 +0300

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,7 +6,7 @@ function Projects({ projectsData }: { projectsData: ProjectType[] }) {
   return (
     <section id="projects" className="max-w-7xl mx-auto px-5 mt-24">
       <h2 className="text-white text-5xl  font-semibold">Featured projects</h2>
-      <div className="grid mt-10 lg:grid-cols-3 justify-items-center md:grid-cols-2 gap-y-20 gap-x-5">
+      <div className="grid mt-10 lg:grid-cols-3 items-start justify-items-center md:grid-cols-2 gap-y-20 gap-x-5">
         {projectsData?.map((project) => {
           return <Project key={project._id} project={project} />;
         })}
