f743dc58e19a129b35ab2ad8edc44346d920ef92
MikeXado <mike.gurin21@gmail.com>
2024-09-10 21:55:41 +0300

    refactor logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Logo({ logo }: { logo: string }) {
   const com = logo.substring("gurin-dev".length);
-  const name = logo.substring(0, 8);
+  const name = logo.substring(0, 9);
 
   return (
     <Link href="/" className="text-white text-lg font-extrabold">
